937302b96a87368745b10ddf1c88ecf1e7e297dd
dakshFrontend <dakshmrigwani@gmail.com>
2023-12-15 11:05:44 +0530

    appedn file done

--- 01_read-file/index.js
+++ 01_read-file/index.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+
+// path is available to do or target the file
 const path = require('path')  // path should be always on top
 
 // without string it creates a buffer data that is cannot read
@@ -26,17 +28,23 @@ process.on('Uncaught exception' , err => {
 })
 
 
-// path is available to do or target tne file
 
-
+// always check file precisely on terminal
 
 fs.readFile('./file/starter.txt', (err, data) => {
     if (err) throw err;
     console.log(data.toString());
 });
 
-
+// in the fs.writeFile function call. The fs.writeFile function requires three arguments: the file path, the data to be written, and a callback function that will be called once the writing is complete or an error occurs.
 fs.writeFile(path.join(__dirname, 'file', 'reply.txt'), 'Hello, this is the content to be written!', (err) => {
     if (err) throw err;
     console.log('File has been written successfully.');
+});
+
+
+//append file create a file which doesn't exist and modify the existing file as well
+fs.appendFile(path.join(__dirname, 'file', 'test.txt'), 'testing test!', (err) => {
+    if (err) throw err;
+    console.log('append has been written successfully.');
 });
