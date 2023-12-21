d9ab00798a25739a8aa30a4994fed1ef9e64b083
dakshFrontend <dakshmrigwani@gmail.com>
2023-12-21 15:39:59 +0530

    just corrected

--- basic-stuff/file-list-system/index.js
+++ basic-stuff/file-list-system/index.js
@@ -4,21 +4,16 @@ const fs = require("fs");
 // fs.writeFileSync("random.txt", "this is random file"); // this is wrong method it will create file not on the folder
 const path = require("path");
 
-
 // const dirPath = path.join(__dirname, "file-list-system"); 
 
 // it is for choosing directory if you weren't there
 
-
-
-
 for (i = 0; i <= 5; i++) {
-
-
 
 //   fs.writeFileSync("random1.txt", "random text"); // for simple name
 //   fs.writeFileSync("random" + i + ".txt", "random text"); // to dynamic name or do this
   // fs.writeFileSync(`random${i}.txt`, "random text");
-  // and all add dir name to target the directory
+  // and  add dirdirPath name to target the directory and if you're in directory then you don't neeed to add directory
   fs.writeFileSync(`random${i}.txt`, "random text");
+
 }
