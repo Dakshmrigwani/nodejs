0dc2846bf9551e3e6c754031841c4b281779ae75
dakshFrontend <dakshmrigwani@gmail.com>
2023-12-26 18:48:06 +0530

    added routing and write html in node

--- 02-Express/index.js
+++ 02-Express/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express(); // we written due to bcsit is not in excecutable state
 app.get("", (req, res) => {
   // it is for home page
-  res.send("Hello this is Home Page");
+  // res.send("Hello this is Home Page"); // the res send one at a time
+  res.send("<h2>Hello</h2>")
+  console.log("data sent by" , req.query.name) // this is ho we get the data from the browser
 });
 app.get("/about", (req, res) => {
   res.send("Hello this is About Page");
